87f2708952d108e9b6b41d0530de5d556bfaf8ec
Anderson Borba <andersonhborba@hotmail.com>
2019-01-31 21:07:04 -0500

    created the first app

---
+++ App1/index.js
@@ -0,0 +1,13 @@
+module.exports = (req, res) =>{
+    res.end(site);
+}
+
+let site = 
+`
+<html>\n
+  <body>\n
+    <h1>My first now Deployment</h1>\n
+    <p> it was successful </p>\n
+  </body>\n
+</html>
+`
